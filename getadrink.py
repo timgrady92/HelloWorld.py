84d4a1324f61940c78880457349087b2dc453067
Tim Grady <88202679+timgrady92@users.noreply.github.com>
2023-11-13 22:18:40 -0500

    Update getadrink.py
    Added the "time" module. Used the sleep function to delay responses for a more interactive experience.

--- getadrink.py
+++ getadrink.py
@@ -1,3 +1,5 @@
+import time
+
 name = input("What is your name? ")
 print("Hello,", name)
 age = int(input("How old are you? "))
@@ -5,11 +7,31 @@ if age >= 21:
     choice = input("Would you like to get a beer? Yes or no? ")
     if choice.lower() == "yes":
         print("Alright, let's have a drink!")
+        time.sleep(1)
+        print("Glug...")
+        time.sleep(1)
+        print("Glug...")
+        time.sleep(1)
+        print("Glug...")
+        time.sleep(2)
+        print("That was delightful.")
     else:
         print("Okay, I won't take it personally.")
 else:
     choice = input("Would you like to get a soda? Yes or no? ")
     if choice.lower() == "yes":
         print("Alright, let's get some McDonald's Sprite!")
+        time.sleep(1)
+        print("Glug...")
+        time.sleep(1)
+        print("Glug...")
+        time.sleep(1)
+        print("Glug...")
+        time.sleep(2)
+        print("That was delightful.")
     else:
         print("Okay, I'll have some Sprite by my lonesome.")
+        
+time.sleep(2)
+
+print("Back at it. See you next time.")
