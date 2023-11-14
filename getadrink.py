591cc1546c7c8379c5b5e9e36e26e24376b50d83
Tim Grady <88202679+timgrady92@users.noreply.github.com>
2023-11-14 00:00:35 -0500

    Update getadrink.py
    Defined the glug() function to reduce clutter in the script. Defined functions are an excellent way of implementing the same lines of code throughout a script with ease.

--- getadrink.py
+++ getadrink.py
@@ -1,4 +1,13 @@
 import time
+def glug():
+	time.sleep(1)
+	print("Glug...")
+	time.sleep(1)
+	print("Glug...")
+	time.sleep(1)
+	print("Glug...")
+	time.sleep(2)
+	print("That was delightful.")
 
 name = input("What is your name? ")
 print("Hello,", name)
@@ -7,28 +16,14 @@ if age >= 21:
     choice = input("Would you like to get a beer? Yes or no? ")
     if choice.lower() == "yes":
         print("Alright, let's have a drink!")
-        time.sleep(1)
-        print("Glug...")
-        time.sleep(1)
-        print("Glug...")
-        time.sleep(1)
-        print("Glug...")
-        time.sleep(2)
-        print("That was delightful.")
+        glug()
     else:
         print("Okay, I won't take it personally.")
 else:
     choice = input("Would you like to get a soda? Yes or no? ")
     if choice.lower() == "yes":
         print("Alright, let's get some McDonald's™ Sprite™!")
-        time.sleep(1)
-        print("Glug...")
-        time.sleep(1)
-        print("Glug...")
-        time.sleep(1)
-        print("Glug...")
-        time.sleep(2)
-        print("That was delightful.")
+        glug()
     else:
         print("Okay, I'll have some Sprite™ by my lonesome.")
         
