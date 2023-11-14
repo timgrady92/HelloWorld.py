f4a9ae64e62a24adb7d94314233bb24f6c023e49
Tim Grady <88202679+timgrady92@users.noreply.github.com>
2023-11-13 22:06:53 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,15 @@
 name = input("What is your name? ")
-print("Hello, " name )
+print("Hello,", name)
+age = int(input("How old are you? "))
+if age >= 21:
+    choice = input("Would you like to get a beer? Yes or no? ")
+    if choice.lower() == "yes":
+        print("Alright, let's have a drink!")
+    else:
+        print("Okay, I won't take it personally.")
+else:
+    choice = input("Would you like to get a soda? Yes or no? ")
+    if choice.lower() == "yes":
+        print("Alright, let's get some McDonald's Sprite!")
+    else:
+        print("Okay, I'll have some Sprite by my lonesome.")
